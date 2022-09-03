e9872963a77032d28c108d8ac8b35e373ccce7be
Biyeon <54989406+ByuN0-0@users.noreply.github.com>
2022-09-04 01:44:02 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int q,k,r,b,n,p;
+  cin>>q>>k>>r>>b>>n>>p;
+  q=1-q;
+  k=1-k;
+  r=2-r;
+  b=2-b;
+  n=2-n;
+  p=8-p;
+  cout<<q<<" "<<k<<" "<<r<<" "<<b<<" "<<n<<" "<<p<<"\n";
+  return 0;
+}
