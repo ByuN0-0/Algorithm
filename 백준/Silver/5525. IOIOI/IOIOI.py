e9a3db05083e04fb6c1f83f46cdef7f74897e5ed
BiYeon <54989406+ByuN0-0@users.noreply.github.com>
2024-03-12 23:03:15 +0900

    [Silver I] Title: IOIOI, Time: 360 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,28 +3,19 @@ import sys
 n = int(sys.stdin.readline())
 length = int(sys.stdin.readline())
 line = sys.stdin.readline()
+i = 0
 cnt = 0
 ans = 0
-for i in range(0,(length-(2*n))):
-  for j in range(0,2*n+1):
-    #print("i는 ", i)
-    #print("j는 ", j)
-    if i+j<length:
-      #print("i+j<length임")
-      if j%2==0: # 문자열이 I일때
-        #print("j%2==0임")
-        if line[i+j]=="I":
-          cnt+=1
-        else:
-          break
-      else: #문자열이 O일때
-        #print("j%2==1임")
-        if line[i+j]=="O":
-          cnt+=1
-        else:
-          break
-    #print("\n")
-  if cnt==(2*n+1):
-    ans+=1
-  cnt = 0
+
+while i < (length-1):
+  if line[i:i+3] == "IOI":
+    i +=2
+    cnt +=1
+    if cnt == n:
+      ans += 1
+      cnt -= 1
+  else:
+    i+=1
+    cnt = 0
+
 print(ans)
