4836554ed23bfcf8fae45cec6095f84c0a1de57c
BiYeon <54989406+ByuN0-0@users.noreply.github.com>
2024-05-17 21:27:35 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void){
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  //
+  int a, b;
+  cin >> a;
+  cin >> b;
+  cout << a*b;
+}
